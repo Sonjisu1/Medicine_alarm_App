aa1c68c8e80480c602fe01b716587adbf3e3da8e
Jisu <jknm0310@naver.com>
2020-02-06 16:53:40 +0900

    Tablayout and Viewpager
    tablayout을 통한 fragment 교체

--- app/src/main/java/com/example/medicine_alarm/MainActivity.java
+++ app/src/main/java/com/example/medicine_alarm/MainActivity.java
@@ -1,14 +1,61 @@
 package com.example.medicine_alarm;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    private TabFragment tabFragment; //tab을 구현하기위한 fragment
+     private Frag2 frag2;
+    private BottomNavigationView bottomNavigationView;
+    private FragmentManager fm;
+    private FragmentTransaction ft;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.list:          //menu 안에 item 중 id가 list인 이미지를 클릭하면
+                        setFrag1(0);
+
+                        break;
+                    case R.id.person1:       //menu 안에 item 중 id 가 person1인 이미지를 클릭하면
+                        setFrag1(1);
+                        break;
+                }
+                return true;
+            }
+        });
+        tabFragment = new TabFragment();
+        frag2 = new Frag2();
+
+
+        setFrag1(0);         //첫 화면
+
+    }
+    public  void setFrag1(int n) {
+        fm = getSupportFragmentManager();
+        ft = fm.beginTransaction(); //프레그멘트 교체가 일어날때
+        switch (n) {
+            case 0:
+                ft.replace(R.id.frame, tabFragment);// TabFragment 레이아웃으로 교체
+                ft.commit();
+                break;
+            case 1:
+                ft.replace(R.id.frame, frag2);    //frag2 레이아웃으로 교체
+                ft.commit();
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/example/medicine_alarm/TabFragment.java
@@ -0,0 +1,36 @@
+package com.example.medicine_alarm;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class TabFragment extends Fragment {
+
+    //Tablayout 구현하는 큰 틀
+    //그 안에서 Frag1,Frag2 가 replace됨
+    private FragmentPagerAdapter fragmentPagerAdapter;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.tabfragment,container,false);
+        ViewPager viewPager = (ViewPager) view.findViewById(R.id.viewpager);
+
+        fragmentPagerAdapter = new ViewPagerAdapter(getChildFragmentManager()); //프레그먼트안에서 프레그먼트들을 불러올때
+        TabLayout tabLayout = view.findViewById(R.id.tabLayout2);
+        viewPager.setAdapter(fragmentPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+
+
+        return view;
+    }
+}
