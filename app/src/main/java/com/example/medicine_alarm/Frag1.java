135142069847242e0cd275e7f0fe95aadbef7732
Jisu <jknm0310@naver.com>
2020-02-08 20:11:50 +0900

    Recyclerview Click event
    리사이클러뷰 클릭 시 약을 추가하기 위한 Activity창
    열림

--- app/src/main/java/com/example/medicine_alarm/Frag1.java
+++ app/src/main/java/com/example/medicine_alarm/Frag1.java
@@ -1,13 +1,17 @@
 package com.example.medicine_alarm;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CalendarView;
 import android.widget.FrameLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -59,6 +63,27 @@ public class Frag1 extends Fragment {
 
         addItem("타이레놀"); //아이템 추가
 
+        //리사이클러뷰 클릭 이벤트
+        recyclerView.addOnItemTouchListener(new RecyclerTouchListener(getActivity(), recyclerView, new ClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+                //  ListViewItem listViewItem =  list.get(position);
+
+                Intent intent = new Intent(getActivity(), AddMedicine.class);
+
+                startActivity(intent);
+                // Toast.makeText(getContext(),"클릭",Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onLongClick(View view, int position) {
+                Toast.makeText(getContext(),"롱클릭",Toast.LENGTH_SHORT).show();
+
+            }
+        }));
+
+
 
         return view;
     }
@@ -83,6 +108,56 @@ public class Frag1 extends Fragment {
         list.add(item);           //데이터 추가
 
 
+    }
+
+
+    //리사이클러뷰 클릭 이벤트를 위한 인터페이스 및 클래스
+    public interface ClickListener{
+        void onClick(View view, int position);
+        void onLongClick(View view,int position);
+
+
+    }
+    public  static class RecyclerTouchListener implements RecyclerView.OnItemTouchListener{
+
+        private GestureDetector gestureDetector;
+        private ClickListener clickListener;
+
+        public  RecyclerTouchListener(Context context,final RecyclerView recyclerView,final ClickListener clickListener){
+            this.clickListener = clickListener;
+            gestureDetector = new GestureDetector(context,new GestureDetector.SimpleOnGestureListener(){
+
+                public boolean onSingleTapUp(MotionEvent e){
+                    return true;
+                }
+                public void onLongPress(MotionEvent e){
+                    View child = recyclerView.findChildViewUnder(e.getX(),e.getY());
+                    if(child != null && clickListener !=null){
+                        clickListener.onLongClick(child,recyclerView.getChildAdapterPosition(child));
+                    }
+                }
+            });
+        }
+
+        @Override
+        public boolean onInterceptTouchEvent(@NonNull RecyclerView rv, @NonNull MotionEvent e) {
+            View child = rv.findChildViewUnder(e.getX(),e.getY());
+            if(child != null && clickListener != null && gestureDetector.onTouchEvent(e)){
+                clickListener.onClick(child,rv.getChildAdapterPosition(child));
+            }
+
+            return false;
+        }
+
+        @Override
+        public void onTouchEvent(@NonNull RecyclerView rv, @NonNull MotionEvent e) {
+
+        }
+
+        @Override
+        public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+
+        }
     }
 
 }
